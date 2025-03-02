4fbd82a71bd02ab0e9c5c22c3116acf4d2c30984
SARKEL1 <144318275+SARKEL1@users.noreply.github.com>
2025-03-02 16:49:57 +0100

    zad1

---
+++ src/app/contact/page.tsx
@@ -0,0 +1,9 @@
+export default function Contact() {
+    return (
+      <div>
+        <h1>Kontakt</h1>
+        <p>Skontaktuj się z nami pod adresem: kontakt@firma.xyz</p>
+      </div>
+    );
+  }
+  
---
+++ src/app/not-found.tsx
@@ -0,0 +1,4 @@
+export default function NotFound() {
+    return <h1>Strona nie znaleziona</h1>;
+  }
+  
